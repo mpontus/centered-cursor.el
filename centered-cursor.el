8add975fdf62cad1e2c9e5bb46e4767ed8f7ea4f
Michael Pontus <m.pontus@gmail.com>
2022-06-19 06:56:41 +0300

    prevent window from scrolling past the bottom

--- centered-cursor.el
+++ centered-cursor.el
@@ -1,6 +1,5 @@
 ;;; centered-cursor.el --- recenter cursor automatically -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015  Michael Pontus
 
 ;; Author: Michael Pontus <m.pontus@gmail.com>
 ;; Keywords: convenience
@@ -29,7 +28,7 @@
 ;; from scrolling past certain point, are handled by forcing the
 ;; cursor to recenter within window before and after adjusting it.
 
-;;; Code:
+;;; Cod
 
 ;;;###autoload
 (define-minor-mode centered-cursor-mode
@@ -38,28 +37,33 @@
         (add-hook 'post-command-hook #'centered-cursor-recenter nil 'local)
       (remove-hook 'post-command-hook #'centered-cursor-recenter 'local)))
 
-;;;###autoload
+;;;###au toload
 (define-global-minor-mode global-centered-cursor-mode
     centered-cursor-mode centered-cursor-mode)
 
 (defun centered-cursor-recenter ()
   "Center the point while keeping window within buffer contents."
-  ;; scroll-commands such as <C-v>, <M-v> change the window scroll
-  ;; and only move cursor by side effect.
-  ;;
-  ;; To ensure that cursor stays remains centered we need to change
-  ;; its position and recenter in case it stopped on EOB.
-  (when (function-get this-command 'scroll-command)
-    (move-to-window-line (ceiling (/ (window-screen-lines) 2))))
-  (recenter))
-
-(defun turn-on-centered-cursor ()
-  "Unconditionally turn on Centered Cursor mode."
-  (centered-cursor-mode 1))
-
-(defun turn-offcentered ()
-  "Unconditionally turn off Centered Cursor mode."
-  (centered-cursor-mode -1))
+  (if (function-get real-this-command 'scroll-command)
+      ;; Scroll command move visible part of the buffer instead of the cursor.
+      ;; Therefore we need to recenter the cursor after chaning window position.
+      (when (and (> (window-start) 1) (< (window-end) (point-max)))
+        (move-to-window-line (ceiling (/ (window-screen-lines) 2))))
+    (when (eq (current-buffer) (window-buffer))
+      (recenter))
+    (let* ((shown-height (cdr (window-text-pixel-size (selected-window)
+                                                      (window-start)
+                                                      (window-end))))
+           (dead-height (- (window-pixel-height) shown-height))
+           (shown-lines (count-lines (window-start) (window-end) t))
+           (line-height (if (zerop shown-height) 0
+                          (/ shown-height shown-lines)))
+           (window-lines (if (zerop line-height) (window-height)
+                           (/ (window-pixel-height) line-height)))
+           (dead-lines (- window-lines shown-lines)))
+      ;; (message "dead-lines: %s, window-lines: %s, line-height: %s"
+      ;;          dead-lines window-lines line-height)
+      (scroll-down (if (zerop shown-height) 0
+                     (max 0 (- dead-lines 3)))))))
 
 (provide 'centered-cursor)
 ;;; centered-cursor.el ends here
